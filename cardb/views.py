645c316897927b14dcab3293e349a8a5e9c15250
cqggyy <cqcar@outlook.com>
2017-10-10 23:34:51 -0400

    model update

--- cardb/views.py
+++ cardb/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from .models import Manufacturer
+from .models import Manufacturer, Carmodel
+
 
 # Create your views here.
 def index(request):
     manufacturer_list = Manufacturer.objects.all().order_by('id')
-    return render(request, 'cardb/index.html', context={'manufacturer_list': manufacturer_list})
+    carmodel_list = Carmodel.objects.all().order_by('id')
+    return render(request, 'cardb/index.html', context={'manufacturer_list': manufacturer_list, 'carmodel_list': carmodel_list})
