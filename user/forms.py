95c7821150d4093b9b06177581d7888a19326aad
cqggyy <cqcar@outlook.com>
2017-10-16 23:32:13 -0400

    login message update

--- user/forms.py
+++ user/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 
 class LoginForm(forms.Form):
-    username = forms.CharField(required=True)
-    password = forms.CharField(required=True, min_length=6)
+    username = forms.CharField(label='用户名:', required=True)
+    password = forms.CharField(label='密码:',required=True, min_length=6)
